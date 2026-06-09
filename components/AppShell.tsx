df21892b8ae10ecbe3f7a0e37035d9e8c5e2c206
Julien Payet <payetjulien.97450@gmail.com>
2026-06-09 21:38:47 +0400

    Fix: contient le scroll horizontal des tableaux (min-w-0) pour ne pas elargir la page

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -12,10 +12,12 @@ export function AppShell({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <SideNav />
-      <div className="flex min-h-screen flex-1 flex-col">
+      {/* min-w-0 : autorise la colonne à rétrécir pour que le scroll horizontal
+          reste contenu dans les tableaux (sinon toute la page déborde). */}
+      <div className="flex min-h-screen min-w-0 flex-1 flex-col">
         <Header />
         {/* pb-20 mobile : laisse la place à la BottomNav fixe. */}
-        <main className="flex-1 pb-20 md:pb-6">{children}</main>
+        <main className="min-w-0 flex-1 pb-20 md:pb-6">{children}</main>
       </div>
       <BottomNav />
     </div>
